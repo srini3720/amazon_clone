638515721ef03af9ac803a58e044f56b52b0c543
DEATH KNELL (LOCAL) <SRINI3720@GMAIL.COM>
2021-07-15 13:45:14 +0530

    final

--- src/Home.js
+++ src/Home.js
@@ -23,7 +23,7 @@ function Home() {
         />
         <Product
           id="51694526"
-          title="Harry Potter "
+          title="The Harry Potter "
           price={299}
           image="https://images-na.ssl-images-amazon.com/images/I/51ifu1aebKL._SX332_BO1,204,203,200_.jpg"
           rating={4}
